759293db8247b7996857cb0ccd52df82e01103d4
lqduongdev <ridbiP-xibmo8-tamkaq>
2021-01-21 17:47:16 +0700

    Config Reanimate

--- android/app/src/main/java/com/freetimeanimation/MainApplication.java
+++ android/app/src/main/java/com/freetimeanimation/MainApplication.java
@@ -2,15 +2,17 @@ package com.freetimeanimation;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -36,6 +38,12 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+          @Override
+          protected JSIModulePackage getJSIModulePackage() {
+              return new ReanimatedJSIModulePackage();
+          }
+
       };
 
   @Override
